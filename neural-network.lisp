3c051caa064b008f0326614d122e9876cee9e4e6
ccQpein <ccqpein@protonmail.com>
2016-05-12 17:25:03 -0400

    did a bit work

--- neural-network.lisp
+++ neural-network.lisp
@@ -3,4 +3,12 @@
 (load "./logistic-regression.lisp")
 (in-package  #:neural-network)
 
-
+(defun computate-single-layer (xArray theta)
+  "theta is a matrix"
+  (let* ((rowNum (1- (elt (array-dimensions theta) 0)))
+         (nextLayer (make-array (+ rowNum 2) :initial-contents
+                               (append '(1)
+                                     (loop for i from 0 to rowNum collect
+                                          (array-multiply xArray (array-slice theta i)))))))
+    nextLayer))
+    
