74d2a835e8332b707f5f01dd71584d5952667e3e
ccQpein <ccqpein@protonmail.com>
2016-05-27 13:46:07 -0400

    keep working

--- K-means.lisp
+++ K-means.lisp
@@ -9,14 +9,35 @@
         (print "You need make sure arrays length be same")
         (sqrt (loop for i from 0 to (1- len) sum
                    (expt (- (elt p1 i) (elt p2 i)) 2))))))
-        
-                 
+
+(defmacro *min (l)
+  (let ((ll (gensym)))
+  `(let ((,ll (coerce ,l 'list)))
+     (min ,@ll)
+     )))
+
+(defun org-matrix (matrix arrayList)
+  (let* ((len (length arrayList))
+         (arraysList (make-list len :initial-element '())))
+    (loop for i from 0 to (1- (array-dimension matrix 0)) do
+         (let* ((thisArray (array-slice matrix i))
+                (distanceList (coerce (loop for a from 0 to (1- len) collect
+                                   (point-distance thisArray (nth a arrayList))) 'list))
+                ;(minVal (*min distanceList))
+                ;(po (position minVal distanceList)))
+                )
+                                        ;(setf (nth po arraysList) (append (nth po arrayList) thisArray))
+           (print (type-of distanceList))
+           (print distanceList)
+                ))))
+
+#|
 (defun K-mean (matrix K &key (iterTime 1000))
   "matrix is input matrix included all train data set, K is cluster number"
   (let ((result '())   ;result is a list for all cluster center point
         (rowNum (array-dimension matrix 0))
-        (colNum (array-dimension matrix 1)))
-    (dotimes (tt iterTime)
-      (let ((initKArray (loop for i from 0 to (1- K) collect
+        (colNum (array-dimension matrix 1))
+        (initKArray (loop for i from 0 to (1- K) collect
                              (array-slice matrix (random rowNum *random-state*)))))
-        
+    (do ((i 1 (incf i))
+         ))))|#
