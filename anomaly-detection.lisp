cad411b8b015a4da933a49f690ebdee6cc8aaba6
ccQpein <ccqpein@protonmail.com>
2016-06-20 18:44:21 -0400

    a bit work

--- anomaly-detection.lisp
+++ anomaly-detection.lisp
@@ -4,4 +4,16 @@
 
 
 ;; Can use toolsbox:points-average
-(defun 
+(defun standard-deviation (numlist)
+  "return the value include the average and standard deviation of the numlist"
+  (let* ((sum (apply #'+ numlist))
+         (len (length numlist))
+         (aver (/ sum len))
+         (SD (sqrt (loop for i in numlist sum
+                       (expt (- i aver) 2)))))
+    (list aver SD)))
+
+
+(defun anomaly-detection (matrix)
+  "as usual, the input var is matrix include every attributions of sample and get the average and the standard deviation"
+  
