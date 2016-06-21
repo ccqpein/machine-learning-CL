7e129abd73b48e525e387ead46909c57aae040fb
ccQpein <ccqpein@protonmail.com>
2016-06-21 00:17:27 -0400

    add new tool marco

--- anomaly-detection.lisp
+++ anomaly-detection.lisp
@@ -3,7 +3,6 @@
 (in-package #:anomaly-detection)
 
 
-;; Can use toolsbox:points-average
 (defun standard-deviation (numlist)
   "return the value include the average and standard deviation of the numlist"
   (let* ((sum (apply #'+ numlist))
@@ -13,20 +12,24 @@
                        (expt (- i aver) 2)))))
     (list aver SD)))
 
-
+(defmacro array-slice-col (m i)
+  (with-gensyms (mm ii)
+    `(let* ((,mm ,m)
+            (,ii ,i)
+            (dimens (array-dimensions ,mm))
+            (rowNum (nth 0 dimens)))
+       (loop for i from 0 to (1- rowNum) collect
+            (aref ,mm i ,ii)))))
 
 
 (defun anomaly-detection (matrix)
   "as usual, the input var is matrix include every attributions of sample and get the average and the standard deviation"
   (let* ((matrixDim (array-dimensions matrix))
-         (rowNum (nth 0 matrixDim))
          (colNum (nth 1 matrixDim))
          (result '()))
     (do* ((colIndex 0 (1+ colIndex))
-          (thisCol (loop for i from 0 to (1- rowNum) collect
-                        (aref matrix i colIndex))
-                   (loop for i from 0 to (1- rowNum) collect
-                        (aref matrix i colIndex))))
+          (thisCol (array-slice-col matrix colIndex)
+                   (array-slice-col matrix colIndex)))
          ((= colIndex (1- colNum))
           (aappend result (standard-deviation thisCol)))  ;last time
       ;(print thisCol) (print colIndex) (print (standard-deviation thisCol))
