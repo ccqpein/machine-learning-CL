e97f8e81ec63bf637432096f3568e8bc26785745
ccQpein <ccqpein@protonmail.com>
2016-06-25 20:30:12 -0400

    a bit

--- recommender-systems.lisp
+++ recommender-systems.lisp
@@ -1,3 +1,9 @@
 (load "./package.lisp")
 (load "./toolsbox.lisp")
 (in-package #:recommender-systems)
+
+
+;; Assume r(r, j) is 1, that means user j has rated movie i. Kinds of bool
+;; This algorithm looks like required Gradient Descent
+
+
