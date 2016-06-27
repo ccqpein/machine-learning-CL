4711662195faa03eed9ee0c8dbc7da54a64dc93a
ccQpein <ccqpein@protonmail.com>
2016-06-27 19:57:05 -0400

    thinking...

--- recommender-systems.lisp
+++ recommender-systems.lisp
@@ -6,4 +6,9 @@
 ;; Assume r(r, j) is 1, that means user j has rated movie i. Kinds of bool
 ;; This algorithm looks like required Gradient Descent
 
+;;; Given parameter vector for user to learn feature vector for movie
+;;; Assume the matrix's row is movie, row is user's data
 
+(defun get-feature-vector (matrix mIndex)
+  "matrix is users' rating form, mIndex means which movie be learned"
+  
