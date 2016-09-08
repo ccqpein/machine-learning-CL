36ceb279e5d300076d5795db502523c7085eb80a
ccQpein <ccqpein@protonmail.com>
2016-09-08 19:00:53 -0400

    a bit change

--- toolsbox.lisp
+++ toolsbox.lisp
@@ -209,12 +209,12 @@
        ,l)))
 
 (defmacro sigma ((&rest exp) indPara paraList times)
-  "Do simga calculation, for example:
-(sigma (+ 1 2) 1 (2 3 4) 2) => (+ (+ 2 2) (+ 3 2))"
-  `(+ ,@(loop for tt from 0 to (1- times)
-           for expT = (copy-list exp)
-           do (setf (nth indPara expT) (nth tt paraList))
-           collect expT)))
+  "Do simga calculation, for example if paraExp = '(2 3): (sigma (+ 1 2) 1 paraExp 2) => (+ (+ 2 2) (+ 3 2))"
+  (let ((para (eval paraList)))
+      `(+ ,@(loop for tt from 0 to (1- times)
+               for expT = (copy-list exp)
+               do (setf (nth indPara expT) (nth tt para))
+               collect expT))))
 
 ;;; The marco below come from http://stackoverflow.com/questions/39048561/issues-when-write-loop-collect-in-macro/39052748#39052748, but it works not follow the purpose.
 
